a50c28aae3d35207d87ba33e42f2e805864d38c8
伟哥 <zhouzw@weigedeMacBook-Pro.local>
2017-08-19 20:38:47 +0800

    Initial Commit

---
+++ VGAutoScrollDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VGAutoScrollDemo
+//
+//  Created by 伟哥 on 2017/8/19.
+//  Copyright © 2017年 vege. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VGAutoScrollDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VGAutoScrollDemo
+//
+//  Created by 伟哥 on 2017/8/19.
+//  Copyright © 2017年 vege. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
